7e8e0dfec15845e8478a2726d31143fe61346b26
LukasThiRodrigues <lukasthi7@gmail.com>
2022-10-17 16:03:05 -0300

    update

---
+++ src/app/core/components/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SignComponent } from './pages/sign/sign.component';
+
+const routes: Routes = [{
+  path: '',
+  component: SignComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
